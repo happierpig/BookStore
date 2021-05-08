e2d766a9a829d3ad87e146bb73bb3e856ff9d2e2
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-05-08 21:40:39 +0800

    bug--

--- BPlusTree.hpp
+++ BPlusTree.hpp
@@ -62,7 +62,7 @@ struct Key{
     }
 };
 
-template <class Key,class Data,int M = 200,int L = 300>
+template <class Key,class Data,int M = 200,int L = 400>
 class BPlusTree{
 private:
     class basicInfo{
