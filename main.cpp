41f9d135cc3f75daca714d71e2e5c8437ce836a2
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-05-11 20:59:16 +0800

    LRU

--- main.cpp
+++ main.cpp
@@ -7,13 +7,28 @@
 using namespace std;
 int main() {
     initialize();
-//    fstream fin("10.in");
+//    for(int i = 1;i <= 10;++i) {
+//        char name[6];
+//        name[0] = i + '0';name[1] = '.';name[2] = 'i';name[3] ='n';name[4] = 0;
+//        fstream fin(name);
+//        string token;
+//        while (getline(fin, token)) {
+//            try {
+//                parseStatement(token);
+//            } catch (int &x) {
+//                if (x == 1) { return 0; }
+//            }
+//            catch (...) {
+//                cout << "Invalid" << endl;
+//            }
+//        }
+//    }
     string token;
-    while(getline(cin,token)){
+    while (getline(cin, token)) {
         try {
             parseStatement(token);
-        }catch (int &x){
-            if(x == 1){return 0;}
+        } catch (int &x) {
+            if (x == 1) { return 0; }
         }
         catch (...) {
             cout << "Invalid" << endl;
