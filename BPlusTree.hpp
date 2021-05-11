ea1e369d0b3b9938dcacc23d12983e427e20419c
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-05-11 20:47:04 +0800

    LRU

--- BPlusTree.hpp
+++ BPlusTree.hpp
@@ -631,7 +631,7 @@ private:
 public:
     // interfaces for my B+Tree
     BPlusTree() = delete;
-    explicit BPlusTree(const string & _name):leafDisk(_name + "_leaf.dat",43),nodeDisk(_name + "_node.dat",43){
+    explicit BPlusTree(const string & _name):leafDisk(_name + "_leaf.dat",113),nodeDisk(_name + "_node.dat",113){
         treeInfo = nodeDisk.tellInfo();
     }
     ~BPlusTree(){
