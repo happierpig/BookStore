e8b32a783197ef947b4176ca52dd44b38e87193d
happierpig <zhaoyilong217@sjtu.edu.cn>
2021-05-14 14:45:00 +0800

    test

--- functions.hpp
+++ functions.hpp
@@ -11,7 +11,8 @@ namespace myFunctions{
     int upper_bound(const T * const head,int size,const T & _target){
         if(!size) return 0;
         if(_target < head[0]) return 0;
-        if(_target >= head[size-1]) return size;
+//        if(_target >= head[size-1]) return size;
+        if(head[size-1] < _target || head[size-1] == _target) return size;
         int l = 0,r = size - 1;
         while(l < r){
             int mid = (l+r) >> 1;
@@ -21,18 +22,21 @@ namespace myFunctions{
         return l;
     }
     /*
-     *     0   1   2   3   4                0
-     *   0   1   2   3   4   5             0   1
+     *     0   1   2   3   4
+     *   0   1   2   3   4   5
      */
     template<class T>
     int lower_bound(const T * const head,int size,const T & _target){
         if(!size) return 0;
-        if(_target <= head[0]) return 0;
-        if(_target > head[size-1]) return size-1;
+//        if(_target <= head[0]) return 0;
+//        if(_target > head[size-1]) return size-1;
+        if(_target < head[0] || _target == head[0]) return 0;
+        if(head[size-1] < _target) return size-1;
         int l = 0,r = size - 1;
         while(l < r){
             int mid = (l+r+1) >> 1;
-            if(_target > head[mid]) l = mid;
+//            if(_target > head[mid]) l = mid;
+            if(head[mid] < _target) l = mid;
             else r = mid - 1;
         }
         return l;
